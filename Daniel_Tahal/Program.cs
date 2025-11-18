94f9e9f37eee3f8176c4bee75774260d00e2f55d
Daniel Tahal <24IA25_TAHAL@spsjc.cz>
2025-11-18 09:24:21 +0100

    boxing, unboxing

--- Daniel_Tahal/Program.cs
+++ Daniel_Tahal/Program.cs
@@ -23,6 +23,7 @@ namespace Daniel_Tahal
 
             //Console.WriteLine(pes.Jmeno);
 
+            #region MyRegion
             Console.WriteLine("Vítejte v ZOO!");
             object[] zoo = { pes, kocka, chameleon, pes2 };
 
@@ -45,105 +46,18 @@ namespace Daniel_Tahal
                     c.ResetujBarvu();
                 }
             }
+            #endregion
+
+            
+            object pejsek = (object) pes; //boxing
+            Console.WriteLine(pejsek);
+
+        }
+
+        static void DesifrujPsa(object obj)
+        {
+            Pes hafan = (Pes)obj; //unboxing
+            hafan.Zastekej();
         }
     }
-
-    //class Lev
-    //{
-    //    public int vek = 25;
-       
-        
-    //    public override string ToString()
-    //    {
-    //        return $"Jsem lev, král zvířat a je mi {vek} let.";
-    //    }
-    //}
-
-    ////class Pes
-    ////{
-    ////    public int vek = 7;
-
-    ////    //public void Zastekej()
-    ////    //{
-    ////    //    Console.WriteLine("haf");
-    ////    //}
-
-    ////    //public override string ToString()
-    ////    //{
-    ////    //    return $"Jsem pes, nejlepší přítel člověka a je mi {vek} let.";
-    ////    //}
-    ////}
-
-    //class Mravenec
-    //{
-    //    public int vek = 3;
-    //    public override string ToString()
-    //    {
-    //        return $"Jsem mravenec a unesu několikanásobek své váhy a je mi {vek} dny.";
-    //    }
-    //}
-
-    //class Kočka
-    //{
-    //    public int vek = 2;
-    //    public override string ToString()
-    //    {
-    //        return $"Jsem kočka, mám 9 životů a je mi {vek} let.";
-    //    }
-    //}
-
-    //class Had
-    //{
-    //    public int vek = 3;
-    //    public override string ToString()
-    //    {
-    //        return $"Jsem had a je mi {vek} let";
-    //    }
-    //}
-
-    //class Opice
-    //{
-    //    public int vek = 8;
-    //    public override string ToString()
-    //    {
-    //        return $"Jsem opice a je mi {vek} let";
-    //    }
-    //}
-
-    //class Slon
-    //{
-    //    public int vek = 42;
-    //    public override string ToString()
-    //    {
-    //        return $"Jsem slon, vážím několik tun a je mi {vek} let.";
-    //    }
-    //}
-
-    ////class Chameleon
-    ////{
-    ////    public int vek = 3;
-    ////    public override string ToString()
-    ////    {
-    ////        return $"Jsem chameleon, umím se dobře maskovat a je mi {vek} let.";
-    ////    }
-    ////}
-
-    //class Papoušek
-    //{
-    //    public int vek = 63;
-    //    public override string ToString()
-    //    {
-    //        return $"Jsem papoušek, umím napodobovat zvuky a je mi {vek} let.";
-    //    }
-    //}
-
-    //class Žirafa
-    //{
-    //    public int vek = 27;
-    //    public override string ToString()
-    //    {
-    //        return $"Jsem žirafa, jsem vysoká několik metrů a je mi {vek} let.";
-    //    }
-    //}
-    
 }
